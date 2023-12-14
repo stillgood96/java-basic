002c22ec61cf34b1b671cb7c75387824188f2426
stillgood96 <a787574@naver.com>
2023-12-14 17:09:41 +0900

    생성자 - 오버로딩과 this()

---
+++ src/construct/ConstructMain2.java
@@ -0,0 +1,17 @@
+package construct;
+
+public class ConstructMain2 {
+
+  public static void main(String[] args) {
+
+    MemberConstruct member1 = new MemberConstruct("memberA", 15, 90);
+    MemberConstruct member2 = new MemberConstruct("memberA", 16);
+
+    MemberConstruct[] members = {member1, member2};
+
+    for (MemberConstruct member : members) {
+      System.out.printf("이름: %s, 나이: %d, 성적: %d\n", member.name, member.age, member.grade);
+    }
+
+  }
+}
--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,9 +5,17 @@ public class MemberConstruct {
   int age;
   int grade;
 
+
+  //추가
+  public MemberConstruct(String name, int age) {
+    this(name,age,50);
+  }
+
   public MemberConstruct(String name, int age, int grade) {
     this.name = name;
     this.age = age;
     this.grade = grade;
   }
+
+
 }
