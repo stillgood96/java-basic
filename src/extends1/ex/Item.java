a4749f7af0c7b706259e609cfa0b766aa535d35f
stillgood96 <a787574@naver.com>
2024-01-03 17:41:57 +0900

    문제와 풀이

---
+++ src/extends1/ex/Item.java
@@ -0,0 +1,19 @@
+package extends1.ex;
+
+public class Item {
+  private String name;
+  private int price;
+
+  public Item(String name, int price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public void print() {
+    System.out.printf("이름: %s, 가격: %d \n", name, price);
+  }
+
+  public int getPrice() {
+    return price;
+  }
+}
