b88d10f45d1b977d1645df7cdf618682895e5326
stillgood96 <a787574@naver.com>
2024-01-22 15:45:58 +0900

    다형성 - 역할과 구현 예제1

---
+++ src/poly/car0/K3Car.java
@@ -0,0 +1,20 @@
+package poly.car0;
+
+public class K3Car {
+
+  public void startEngine() {
+    System.out.println("K3Car. startEngine");
+  }
+
+
+  public void offEngine() {
+    System.out.println("K3Car.offEngine");
+  }
+
+  public void pressAccelartor() {
+    System.out.println("K3Car. pressAccelerator");
+  }
+
+
+}
+
